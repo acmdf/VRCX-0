0995fc18cf0ce959e5d6f4ecbc3bd25b602b2228
acmdf <233685277+acmdf@users.noreply.github.com>
2026-09-23 09:50:15 +1000

    Update npm hash

--- flake.nix
+++ flake.nix
@@ -14,10 +14,11 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , rust-overlay
-    , ...
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
     }:
     let
       inherit (nixpkgs) lib;
@@ -27,119 +28,121 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      pkgsFor = lib.genAttrs systems (system:
+      pkgsFor = lib.genAttrs systems (
+        system:
         import nixpkgs {
           localSystem.system = system;
           overlays = [ (import rust-overlay) ];
-        });
+        }
+      );
       srcCargoToml = lib.importTOML ./src-tauri/Cargo.toml;
       rust-version = "1.98.0";
     in
     {
-      packages =
-        lib.mapAttrs
-          (system: pkgs: {
-            vrcx-0 =
-              let
-                rust = pkgs.rust-bin.stable.${rust-version}.default;
+      packages = lib.mapAttrs (system: pkgs: {
+        vrcx-0 =
+          let
+            rust = pkgs.rust-bin.stable.${rust-version}.default;
 
-                rustPlatform = pkgs.makeRustPlatform {
-                  cargo = rust;
-                  rustc = rust;
-                };
-              in
-              rustPlatform.buildRustPackage (finalAttrs: {
-                pname = "vrcx-0";
-
-                src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-                inherit (srcCargoToml.package) version;
-
-                cargoLock.lockFile = ./Cargo.lock;
-
-                npmDeps = pkgs.fetchNpmDeps {
-                  name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
-                  inherit (finalAttrs) src;
-                  hash = "sha256-dISp3e8mIPQm0CEe8PE0rPjeJSSeJ0GoxybE/Q2c77c==";
-                };
-
-                nativeBuildInputs = with pkgs; [
-                  # Pull in our main hook
-                  cargo-tauri.hook
-
-                  # Setup npm
-                  nodejs
-                  npmHooks.npmConfigHook
-
-                  # Make sure we can find our libraries
-                  pkg-config
-                  rustPlatform.bindgenHook
-                  cmake
-                  wrapGAppsHook4
-                ]
-                ++ lib.optionals stdenv.hostPlatform.isLinux [ wrapGAppsHook4 ];
-
-                buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux (with pkgs; [
-                  glib-networking
-                  openssl
-                  webkitgtk_4_1
-                  libayatana-appindicator
-                ]);
-
-                postPatch = ''node <<'NODE'
-                  const fs = require('node:fs');
-
-                  const tauriConfigPath = 'src-tauri/tauri.conf.json';
-                  const linuxConfigPath = 'src-tauri/tauri.linux.conf.json';
-
-                  const tauriConfig = JSON.parse(fs.readFileSync(tauriConfigPath, 'utf8'));
-                  let config = structuredClone(tauriConfig);
-
-                  const linuxConfig = JSON.parse(fs.readFileSync(linuxConfigPath, 'utf8'));
-                  config = {
-                      bundle: {
-                          ...(linuxConfig.bundle ?? {}),
-                          targets: ['appimage'],
-                          createUpdaterArtifacts: false
-                      }
-                  };
-
-                  fs.writeFileSync(linuxConfigPath, JSON.stringify(config, null, 4));'';
-
-                preFixup = ''
-                  gappsWrapperArgs+=(
-                    --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libayatana-appindicator ]}"
-                  )
-                '';
-
-                # Set our Tauri source directory
-                cargoRoot = "";
-                # And make sure we build there too
-                buildAndTestSubdir = "src-tauri";
-
-                passthru.updateScript = pkgs.nix-update-script { };
-
-                meta = {
-                  description = "The fast, lightweight VRCX built with Tauri and Rust.";
-                  license = lib.licenses.gpl3;
-                  platforms = lib.platforms.linux;
-                  mainProgram = "vrcx-0";
-                };
-              });
-
-            default = self.packages.${system}.vrcx-0;
-          })
-          pkgsFor;
-
-      devShells =
-        lib.mapAttrs
-          (system: pkgs: {
-            default = pkgs.mkShell {
-              inputsFrom = [ self.packages.${system}.vrcx-0 ];
-
-              packages = [ pkgs.rust-bin.stable.${rust-version}.default ];
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = rust;
+              rustc = rust;
             };
-          })
-          pkgsFor;
+          in
+          rustPlatform.buildRustPackage (finalAttrs: {
+            pname = "vrcx-0";
+
+            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+            inherit (srcCargoToml.package) version;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            npmDeps = pkgs.fetchNpmDeps {
+              name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
+              inherit (finalAttrs) src;
+              hash = "sha256-h9YihimQoWMBqgZOIdUtXJNQvb+06MRbLf4cHpiVxwY=";
+            };
+
+            nativeBuildInputs =
+              with pkgs;
+              [
+                # Pull in our main hook
+                cargo-tauri.hook
+
+                # Setup npm
+                nodejs
+                npmHooks.npmConfigHook
+
+                # Make sure we can find our libraries
+                pkg-config
+                rustPlatform.bindgenHook
+                cmake
+                wrapGAppsHook4
+              ]
+              ++ lib.optionals stdenv.hostPlatform.isLinux [ wrapGAppsHook4 ];
+
+            buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux (
+              with pkgs;
+              [
+                glib-networking
+                openssl
+                webkitgtk_4_1
+                libayatana-appindicator
+              ]
+            );
+
+            postPatch = ''
+              node <<'NODE'
+                                const fs = require('node:fs');
+
+                                const tauriConfigPath = 'src-tauri/tauri.conf.json';
+                                const linuxConfigPath = 'src-tauri/tauri.linux.conf.json';
+
+                                const tauriConfig = JSON.parse(fs.readFileSync(tauriConfigPath, 'utf8'));
+                                let config = structuredClone(tauriConfig);
+
+                                const linuxConfig = JSON.parse(fs.readFileSync(linuxConfigPath, 'utf8'));
+                                config = {
+                                    bundle: {
+                                        ...(linuxConfig.bundle ?? {}),
+                                        targets: ['appimage'],
+                                        createUpdaterArtifacts: false
+                                    }
+                                };
+
+                                fs.writeFileSync(linuxConfigPath, JSON.stringify(config, null, 4));'';
+
+            preFixup = ''
+              gappsWrapperArgs+=(
+                --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libayatana-appindicator ]}"
+              )
+            '';
+
+            # Set our Tauri source directory
+            cargoRoot = "";
+            # And make sure we build there too
+            buildAndTestSubdir = "src-tauri";
+
+            passthru.updateScript = pkgs.nix-update-script { };
+
+            meta = {
+              description = "The fast, lightweight VRCX built with Tauri and Rust.";
+              license = lib.licenses.gpl3;
+              platforms = lib.platforms.linux;
+              mainProgram = "vrcx-0";
+            };
+          });
+
+        default = self.packages.${system}.vrcx-0;
+      }) pkgsFor;
+
+      devShells = lib.mapAttrs (system: pkgs: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.vrcx-0 ];
+
+          packages = [ pkgs.rust-bin.stable.${rust-version}.default ];
+        };
+      }) pkgsFor;
 
       overlays.default = final: prev: { inherit (self.packages.${prev.system}) vrcx-0; };
     };
